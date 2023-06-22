b55216c6a9037231c03c84cd36619acfff705075
Lyonel Pierce <lyonel@live.com>
2023-06-22 12:15:09 -0400

    Migrations update

--- database/migrations/2023_06_14_205701_create_vendors_table.php
+++ database/migrations/2023_06_14_205701_create_vendors_table.php
@@ -25,7 +25,6 @@ return new class extends Migration
             $table->string('document');
             $table->string('email')->unique();
             $table->string('password');
-            $table->tinyInteger('studio_id');
             $table->tinyInteger('status');
             $table->timestamps();
         });
